87285f820f77ea44e952257cd077e54be98f07f1
felipesirio <felipe.sirio@gmail.com>
2019-07-27 10:01:13 -0300

    Operação de instanciação

--- src/main/java/com/exemplo/aulamc/AulamcApplication.java
+++ src/main/java/com/exemplo/aulamc/AulamcApplication.java
@@ -1,13 +1,30 @@
 package com.exemplo.aulamc;
 
+import com.exemplo.aulamc.domain.Categoria;
+import com.exemplo.aulamc.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class AulamcApplication {
+public class AulamcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulamcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
--- src/main/java/com/exemplo/aulamc/resources/CategoriaResource.java
+++ src/main/java/com/exemplo/aulamc/resources/CategoriaResource.java
@@ -3,14 +3,12 @@ package com.exemplo.aulamc.resources;
 import com.exemplo.aulamc.domain.Categoria;
 import com.exemplo.aulamc.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value="categorias")
