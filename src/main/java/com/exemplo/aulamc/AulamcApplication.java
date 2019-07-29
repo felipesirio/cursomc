5ec20089e3f8621c8b62c92ea38e88598dd00324
felipesirio <felipe.sirio@gmail.com>
2019-07-29 13:52:44 -0300

    Produo e Categoria, muitos pra muitos

--- src/main/java/com/exemplo/aulamc/AulamcApplication.java
+++ src/main/java/com/exemplo/aulamc/AulamcApplication.java
@@ -1,7 +1,9 @@
 package com.exemplo.aulamc;
 
 import com.exemplo.aulamc.domain.Categoria;
+import com.exemplo.aulamc.domain.Produto;
 import com.exemplo.aulamc.repository.CategoriaRepository;
+import com.exemplo.aulamc.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,19 +14,40 @@ import java.util.Arrays;
 @SpringBootApplication
 public class AulamcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AulamcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AulamcApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-	}
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        /*cat1.setProdutos(Arrays.asList(p1, p2, p3));
+        cat2.setProdutos(Arrays.asList(p2));
+
+        p1.setCategorias(Arrays.asList(cat1));
+        p2.setCategorias(Arrays.asList(cat1, cat2));
+        p3.setCategorias(Arrays.asList(cat1));*/
+
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+        cat2.getProdutos().add(p2);
+
+        p1.getCategorias().add(cat1);
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().add(cat1);
+
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
--- src/main/java/com/exemplo/aulamc/resources/CategoriaResource.java
+++ src/main/java/com/exemplo/aulamc/resources/CategoriaResource.java
@@ -3,7 +3,6 @@ package com.exemplo.aulamc.resources;
 import com.exemplo.aulamc.domain.Categoria;
 import com.exemplo.aulamc.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
